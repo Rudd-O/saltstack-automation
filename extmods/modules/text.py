742f048d5ff0c5fe0b260b8a05614fb53f52cfd6
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2022-02-09 00:01:26 +0000

    Quote lists and dicts too.

--- extmods/modules/text.py
+++ extmods/modules/text.py
@@ -1,23 +1,36 @@
 from __future__ import print_function
 
-import pipes
+
+import collections
+
+try:
+    from shlex import quote as mquote
+except ImportError:
+    from pipes import quote as mquote
 from six import string_types
 
 
 def quote(s):
-    return pipes.quote(s)
+    if isinstance(s, dict) or isinstance(s, collections.OrderedDict):
+        lst = [(x, mquote(y)) for x, y in s.items()]
+        return s.__class__(lst)
+    if isinstance(s, list) or isinstance(s, tuple):
+        lst = [mquote(y) for y in s]
+        return lst
+    return mquote(s)
+
+
+def list_to_dict(s):
+    return dict([(x, x) for x in s])
 
 
 def escape_regex(l):
     if isinstance(l, string_types):
         n = []
-        specials = [
-            '/', '.', '*', '+', '?', '|',
-            '(', ')', '[', ']', '{', '}', '\\'
-        ]
+        specials = ["/", ".", "*", "+", "?", "|", "(", ")", "[", "]", "{", "}", "\\"]
         for char in l:
             if char in specials:
-                char = '\\' + char
+                char = "\\" + char
             n.append(char)
         return "".join(n)
     return "|".join(escape_regex(x) for x in l)
