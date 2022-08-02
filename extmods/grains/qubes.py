6d6ebebf3654d8418b55fd55933b1b933d91de1e
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2022-08-02 12:27:40 +0000

    Consider normal machines as updateable too.

--- extmods/grains/qubes.py
+++ extmods/grains/qubes.py
@@ -10,20 +10,20 @@ def qubes():
     with open(os.devnull, "w") as devnull:
         try:
             grains['vm_type'] = subprocess.check_output(['qubesdb-read', '/qubes-vm-type'], stderr=devnull, universal_newlines=True).strip()
-        except subprocess.CalledProcessError as e:
+        except (subprocess.CalledProcessError, FileNotFoundError):
             if os.path.exists("/etc/qubes-release"):
                 grains['vm_type'] = "AdminVM"
         try:
             grains['persistence'] = subprocess.check_output(['qubesdb-read', '/qubes-vm-persistence'], stderr=devnull, universal_newlines=True).strip()
-        except subprocess.CalledProcessError:
+        except (subprocess.CalledProcessError, FileNotFoundError):
             if grains.get('vm_type') == "AdminVM":
                 grains['persistence'] = "full"
         try:
             grains['updateable'] = subprocess.check_output(['qubesdb-read', '/qubes-vm-updateable'], stderr=devnull, universal_newlines=True).strip()
             grains['updateable'] = True if grains['updateable'] == "True" else False
-        except subprocess.CalledProcessError:
-            if grains.get('vm_type') == "AdminVM":
-                grains['updateable'] = True
+        except (subprocess.CalledProcessError, FileNotFoundError):
+            # dom0 or physical machine -- updateable
+            grains['updateable'] = True
     return {'qubes': grains}
 
 
