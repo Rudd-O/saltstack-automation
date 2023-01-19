21306de35ba2c368b55fbcf1f72b71d4b3f2414a
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2023-01-19 19:25:41 +0000

    Make the distro-sync process nicer in terms of disk and CPU.

--- formulas/maint/distupgrade/upgrade/dnf-distro-sync.sh
+++ formulas/maint/distupgrade/upgrade/dnf-distro-sync.sh
@@ -5,7 +5,7 @@ set -xe
 curr=$(cat /.distupgrade | head -1)
 next=$(cat /.distupgrade | tail -1)
 
-stdout=$(dnf distro-sync -y --best --releasever="$next")
+stdout=$(ionice -c 3 nice dnf distro-sync -y --best --releasever="$next")
 changed=no
 if echo "$stdout" | grep -q "Nothing to do." ; then changed=yes ; fi
 echo "$stdout"
