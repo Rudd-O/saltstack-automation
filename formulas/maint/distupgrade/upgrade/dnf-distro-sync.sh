cd90905f48a8ab560f5e653fec21b7a6a56b9843
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2023-02-24 14:46:56 +0000

    Fix distro sync.

--- formulas/maint/distupgrade/upgrade/dnf-distro-sync.sh
+++ formulas/maint/distupgrade/upgrade/dnf-distro-sync.sh
@@ -5,9 +5,12 @@ set -xe
 curr=$(cat /.distupgrade | head -1)
 next=$(cat /.distupgrade | tail -1)
 
-stdout=$(ionice -c 3 nice dnf distro-sync -y --best --releasever="$next")
-changed=no
-if echo "$stdout" | grep -q "Nothing to do." ; then changed=yes ; fi
-echo "$stdout"
+changed=yes
+ret=0
+output=$(ionice -c 3 nice dnf distro-sync -y --best --releasever="$next" 2>&1) || ret=$?
+echo "$output" >&2
+if [ "$ret" != "0" ] ; then exit $ret ; fi
+
+if echo "$output" | grep -q "Nothing to do." ; then changed=no ; fi
 echo
 echo changed=$changed
