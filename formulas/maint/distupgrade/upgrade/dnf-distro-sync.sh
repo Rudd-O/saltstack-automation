24a5f8e3673927819ee78a9d4d64d05509bfd632
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2024-02-29 08:22:44 +0000

    Avoid installing weak deps.

--- formulas/maint/distupgrade/upgrade/dnf-distro-sync.sh
+++ formulas/maint/distupgrade/upgrade/dnf-distro-sync.sh
@@ -9,7 +9,7 @@ echo "This system has OS release $curr and will be upgraded to OS release $next"
 
 changed=yes
 ret=0
-output=$(ionice -c 3 nice dnf distro-sync -y --best --releasever="$next" 2>&1) || ret=$?
+output=$(ionice -c 3 nice dnf distro-sync -y --setopt=install_weak_deps=False --best --releasever="$next" 2>&1) || ret=$?
 echo "$output" >&2
 if [ "$ret" != "0" ] ; then exit $ret ; fi
 
